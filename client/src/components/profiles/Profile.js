2d2cd6e0a96788f633ad8d59ea630473e0a4d904
MeikaFreckelton <meikafreckelton@gmail.com>
2021-01-06 17:13:03 +1100

    profiles display is responsive for desktop

--- client/src/components/profiles/Profile.js
+++ client/src/components/profiles/Profile.js
@@ -71,68 +71,96 @@ const Profile = ({ profile }) => {
     return(
         <div id="profile">
 
-            <div className="profileImageDisplay">
+            <div className="profileImgName">
+                <div className="profileImageDisplay">
+                    <div className="profileImg">
 
-            </div>
-
-            <div className="personalDetails">
-                <div>
-                    <h2 id="username">@{username}</h2>
-                    <h3 id="name">{firstName} {lastName}</h3>
-                    <h4 id="location">{location}</h4>
-                </div>
-                <div className="status">
-                    {status}
-                </div>
-                <div className="experienceDesc">
-                    <div>
-                        <p><span className="bold">{skillLevel}</span> Developer with</p>
-                        <p><span className="bold">{yearsOfExperience}</span> year of experience</p>
                     </div>
-                    <div>
-                        <h2>Interested in:</h2>
-                        <p>
-                            {
-                                interests.map((x, index) => {
-                                    return (`${x}, ` )
-                                })
-                            }  
-                        </p>
 
+                    <div className="status-desktop">
+                        {status}
+                    </div>
+
+                </div>
+
+                <div className="nameLocation">
+                    <h2 id="username">@{username}</h2>
+                    <div className="lastChildren">
+                        <h3 id="name">{firstName} {lastName}</h3>
+                        <h4 id="location">{location}</h4>
                     </div>
                     
                 </div>
+
+                <div className="personalDetails">
+                    <div className="status">
+                        {status}
+                    </div>
+                    <div className="experienceDesc">
+                        <div className="experienceLevel">
+                            <p><span className="bold">{skillLevel}</span> Developer with <span className="bold">{yearsOfExperience}</span> year of experience</p>
+                        </div>
+                        <div>
+                            <h2>Interested in:</h2>
+                            <p>
+                                {
+                                    interests.map((x, index) => {
+                                        return (`${x}, ` )
+                                    })
+                                }  
+                            </p>
+
+                        </div>
+                        
+                    </div>
+                </div>
+
+                
+
+            </div>
+
+
+            <div className="bioAndLinks">
                 <div className="bio">
                     {bio}
                 </div>
-            </div>
-
-            <div className="linksDisplay">
-                <h1>Get in touch</h1>
-                <DisplayLinks correctLinks={correctLinks}/>
-
-            </div>
-
-            <div className="workDisplay">
-                <h1>Current Role</h1>
-                <h2>{currentTitle} at {currentCompany}</h2>
-                <h3>{currentStartDate.slice(0, 4)} - present</h3>
-                {
-                    pastRoles.length > 0 &&
-                    <h1>Past Roles ^</h1>
-                }
                 
-            </div>
+                <div className="linksDisplay">
+                    <h1>Get in touch</h1>
+                    <DisplayLinks correctLinks={correctLinks}/>
 
-            <div className="eduDisplay">
-                <h1>Education</h1>
-                <h2>{recentSchool} ({recentStartDate.slice(0, 4)} - {recentEndDate.slice(0, 4)})</h2>
-                <h3>{recentDegree}</h3>
-                {
-                    pastEducation.length > 0 &&
-                    <h1>Previous Education ^</h1>
-                }
+                </div>
+
             </div>
+            
+            
+            
+
+            
+            <div className="workAndEdu">
+                <div className="workDisplay">
+                    <h1>Current Role</h1>
+                    <h2>{currentTitle} at {currentCompany}</h2>
+                    <h3>{currentStartDate.slice(0, 4)} - present</h3>
+                    {
+                        pastRoles.length > 0 &&
+                        <h1>Past Roles ^</h1>
+                    }
+                    
+                </div>
+
+                <div className="eduDisplay">
+                    <h1>Education</h1>
+                    <h2>{recentSchool} ({recentStartDate.slice(0, 4)} - {recentEndDate.slice(0, 4)})</h2>
+                    <h3>{recentDegree}</h3>
+                    {
+                        pastEducation.length > 0 &&
+                        <h1>Previous Education ^</h1>
+                    }
+                </div>
+
+            </div>
+            
 
             <div className="skillsDisplay">
                 <h1>Technical Skills/Languages</h1>
