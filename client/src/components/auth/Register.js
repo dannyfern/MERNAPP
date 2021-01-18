cefd06f71e89bf1467509a571e156e9149e8633e
MeikaFreckelton <meikafreckelton@gmail.com>
2021-01-19 09:40:58 +1100

    pushing changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,108 +5,83 @@ import { setAlert } from '../../actions/alert';
 import { register } from '../../config/api';
 import './../../styles/Auth.css';
 import PropTypes from 'prop-types';
+import FormInput from './../reusable/FormInput'
 
-
-// use forminput component
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
+
+  // initial form data to register :
   const [formData, setFormData] = useState({
     name:'',
     email:'',
     password:'',
     password2:''
   });
-
   const { name, email, password, password2 } = formData;
 
+  // onchange event to set form data :
   const onChange = c => setFormData({ ...formData, [c.target.name]: c.target.value })
 
+  // password validation : 
   const onSubmit = async c => {
     c.preventDefault();
     if(password !== password2) {
       setAlert('Passwords arent matching', 'danger')
     } else {
       register({ name, email, password });
-        
     }
   };
 
+  // if user is authenticated, redirect to dashboard :
   if(isAuthenticated) {
     return <Redirect to='/dashboard' />
   }
-    
-
-//     return <Fragment>
-//     <section className="signup">
-//     <div className="register">
-//         <h1 className="text-signup">Sign Up</h1>
-//     </div>
-//     <p className="text-signup"><i className=""></i> Create Your Account</p>
-//         <form className="form" onSubmit={c => onSubmit(c)}>
-//           <div className="form-signup">
 
     
   return <Fragment>
     <div className="registerComponent">
-      <div className="bgImage">
+
+      <div className="bgImage"> 
       </div>
 
       <div className="registerPage">
 
-
         <div className="register">
-            <h1 className="large text-primary">Sign Up</h1>
+          <h1 className="large text-primary">Sign Up</h1>
         </div>
 
-      <p className="lead"><i className="fas fa-user"></i>&nbsp; Create Your Account</p>
-        <form className="form register-form" onSubmit={c => onSubmit(c)}>
-          <div className="form-group">
+        <p className="lead"><i className="fas fa-user"></i>&nbsp; Create Your Account</p>
 
-            <input 
-            type='text'
-            placeholder='Name...'
-            name='name' 
-            value={name} 
-            onChange={c => onChange(c)} />
+        <form className="form register-form" onSubmit={c => onSubmit(c)}>
+
+          <div className="form-group">
+            <FormInput placeholder="Name..." name="name" value={name} onChange={c => onChange(c)} />
           </div>
-          <div className="form-signup">
-            <input 
-            type="email" 
-            placeholder="Email Address..." 
-            name="email" 
-            value={email}
-            onChange={c => onChange(c)}  />
+
+          <div className="form-group">
+            <FormInput placeholder="Email Address..." name="email" value={email} onChange={c => onChange(c)} />
           </div>
-          <div className="form-signup">
-            <input
-              type="password"
-              placeholder="Password..."
-              name="password"
-              value={password}
-              onChange={c => onChange(c)} 
-              minLength="6"
-            />
+
+          <div className="form-group">
+            <FormInput type="password" placeholder="Password..." name="password" value={password} onChange={c => onChange(c)} minLength="6" />
           </div>
-          <div className="form-signup">
-            <input
-              type="password"
-              placeholder="Confirm Password..."
-              name="password2"
-              value={password2}
-              onChange={c => onChange(c)} 
-              minLength="6"
-            />
+
+          <div className="form-group">
+            <FormInput type="password" placeholder="Confirm Password..." name="password2" value={password2} onChange={c => onChange(c)} minLength="6" />
           </div>
+
           <div className="ending">
             <input type="submit" className="btn btn-primary" value="Register" />
-
           </div>
-              <p className="my-1">
-                  Already have an account? <Link to="/auth/signin">Sign In</Link>              </p>
-            </form>
-            </div>
-            </div>
-       </Fragment>
+
+          <p className="my-1">
+            Already have an account? <Link to="/auth/signin">Sign In</Link>              
+          </p>
+        </form>
+
+      </div>
+    </div>
+  </Fragment>
 
 }
 
@@ -122,45 +97,3 @@ const mapStateToProps = state => ({
 
 export default connect(mapStateToProps, { setAlert, register })(Register);
 
-// const initialState = {
-//     email: "",
-//     username: "",
-//     password: ""
-// }
-
-// const [userDetails, setUserDetails] = useState(initialState)
-
-
-// function handleChange(event){
-//     const { name, value } = event.target
-//     setUserDetails({
-//         ...userDetails,
-//         [name]: value
-//     })
-// }
-
-// function handleSubmit(event){
-//     event.preventDefault()
-//     // functionality ...
-// }
-
-
-// const newUser = {
-//     name,
-//     email,
-//     password
-// }
-// try {
-//     const config = {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }
-
-//     const body = JSON.stringify(newUser);
-
-//     const res = await axios.post('/api/users', body, config);
-//     console.log(res.data);
-// } catch (err) {
-//     console.error(err.response.data);
-// }
