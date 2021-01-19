51ded85249f47a61a7fb4a5eb6af36d4aa92f559
MeikaFreckelton <meikafreckelton@gmail.com>
2021-01-19 17:25:28 +1100

    profiles styled

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,15 +1,22 @@
 import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import ProfileDisplay from './ProfileDisplay'
 import { getProfileId } from '../../actions/profile'
 
-const Profile = ({ getProfileId, profile: {profile, match} }) => {
-    useEffect(() => {
-        getProfileId(match.params.id)
-    }, [getProfileId]);
+const Profile = ({ getProfileId, match }) => {
+    // const dispatch = useDispatch()
+    console.log(match.params.id)
 
-    // console.log(profiles)
+    // useEffect(() => {
+    //     dispatch(getProfileId(match.params.id))
+    // }, [dispatch, getProfileId, match.params.id]);
+
+    let profiles = useSelector(state => state.profile.profiles)
+
+    console.log(profiles)
+    let profile = profiles.filter(x => x.user._id === match.params.id)[0]
+    console.log(profile)
 
     return (
         <Fragment>
